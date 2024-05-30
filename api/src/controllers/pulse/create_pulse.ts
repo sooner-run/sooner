5ae3f22d5634b84f4dbd4a0ef56769b61fc2cb09
akinloluwami <akinkunmioye42@gmail.com>
2024-05-30 23:53:53 +0100

    write to db

--- api/src/controllers/pulse/create_pulse.ts
+++ api/src/controllers/pulse/create_pulse.ts
@@ -7,11 +7,12 @@ export const create_pulse = async (c: Context) => {
     const user_id = c.get("user_id");
     const body = await c.req.json();
 
-    await db.insert(pulses).values({
+    const pulse = await db.insert(pulses).values({
       user_id,
       ...body,
+      path: body.path.replace("\\", "/"),
     });
-    return c.json({ message: "Pulse created.", user_id }, 201);
+    return c.json({ message: "Pulse created.", pulse }, 201);
   } catch (error) {
     return c.json({ message: "Something went wrong." }, 500);
   }
