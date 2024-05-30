1ad65616b46ef1f033391997fd6e777540eec5f8
akinloluwami <akinkunmioye42@gmail.com>
2024-05-30 12:08:53 +0100

    verify user token and set id to context

--- api/src/middlewares/authenticate_user.ts
+++ api/src/middlewares/authenticate_user.ts
@@ -1,19 +1,23 @@
 import { Context, MiddlewareHandler } from "hono";
 import { getCookie } from "hono/cookie";
+import { verify } from "jsonwebtoken";
 
 export const userAuthMiddleware: MiddlewareHandler = async (
   c: Context,
   next
 ) => {
   try {
-    console.log(c);
     const auth_token = getCookie(c, "sooner.auth-token");
 
     if (!auth_token) {
       return c.json({ message: "Auth token is not present." }, 400);
     }
 
-    console.log(auth_token);
+    const decoded = verify(auth_token, process.env.JWT_SECRET!) as {
+      id: string;
+    };
+
+    c.set("auth.user_id", decoded.id);
 
     await next();
   } catch (error) {
