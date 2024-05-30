0623b326c4d07d9168df1afad5cdb10bcef80d22
akinloluwami <akinkunmioye42@gmail.com>
2024-05-30 23:54:07 +0100

    retrieve all pulses

---
+++ api/src/controllers/pulse/retrieve_all_pulses.ts
@@ -0,0 +1,18 @@
+import { Context } from "hono";
+import { db } from "../../db";
+import { pulses } from "../../db/schema";
+import { eq } from "drizzle-orm";
+
+export const retrieve_all_pulses = async (c: Context) => {
+  try {
+    const _pulses = await db
+      .select()
+      .from(pulses)
+      .where(eq(pulses.user_id, c.get("auth.user_id")));
+
+    return c.json({ pulses: _pulses }, 200);
+  } catch (error) {
+    console.log(error);
+    return c.json({ message: "Something went wrong." }, 500);
+  }
+};
