a043798aaa5a20c4a1105f794fcc1d621d0f6a05
akinloluwami <akinkunmioye42@gmail.com>
2024-05-30 11:40:16 +0100

    /auth /app

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,9 +3,11 @@ import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { prettyJSON } from "hono/pretty-json";
 import { router as auth } from "./routes.auth";
+import { router as app_routes } from "./routes.app";
 import { router } from "./routes";
 import env from "dotenv";
 import { authMiddleware } from "./middlewares/authenticate_api_key";
+import { userAuthMiddleware } from "./middlewares/authenticate_user";
 
 env.config();
 
@@ -18,11 +20,12 @@ app.get("/", (c) => {
   return c.json({ message: "Yoo, bitches!!!!" });
 });
 
-app.route("/auth", auth);
-
+app.use("/app", userAuthMiddleware);
 app.use("/pulse", authMiddleware);
 
 app.route("/", router);
+app.route("/auth", auth);
+app.route("/app", app_routes);
 
 const port = 1716;
 console.log(`Server is running on port http://localhost:${port}`);
