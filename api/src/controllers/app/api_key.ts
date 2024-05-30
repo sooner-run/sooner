adb6757ddd746b3ef65fdf8f64740ea1d54cbd5d
akinloluwami <akinkunmioye42@gmail.com>
2024-05-30 11:40:42 +0100

    get api key

--- api/src/controllers/app/api_key.ts
+++ api/src/controllers/app/api_key.ts
@@ -1,3 +1,9 @@
 import { Context } from "hono";
 
-export const api_key = async (c: Context) => {};
+export const api_key = async (c: Context) => {
+  try {
+    return c.json({ message: "API key gotten." }, 201);
+  } catch (error) {
+    return c.json({ message: "Something went wrong." }, 500);
+  }
+};
