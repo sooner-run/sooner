fae9088187a5faa687ecff767a36449f968ce976
akinloluwami <akinkunmioye42@gmail.com>
2024-05-30 23:55:48 +0100

    removed assignr to var

--- api/src/controllers/pulse/create_pulse.ts
+++ api/src/controllers/pulse/create_pulse.ts
@@ -7,12 +7,12 @@ export const create_pulse = async (c: Context) => {
     const user_id = c.get("user_id");
     const body = await c.req.json();
 
-    const pulse = await db.insert(pulses).values({
+    await db.insert(pulses).values({
       user_id,
       ...body,
       path: body.path.replace("\\", "/"),
     });
-    return c.json({ message: "Pulse created.", pulse }, 201);
+    return c.json({ message: "Pulse created." }, 201);
   } catch (error) {
     return c.json({ message: "Something went wrong." }, 500);
   }
