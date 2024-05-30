453f56f3c13835f5d1d68125a0b12ad9ae347de6
akinloluwami <akinkunmioye42@gmail.com>
2024-05-30 23:53:04 +0100

    /*

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,6 +1,8 @@
 import { Hono } from "hono";
 import { create_pulse } from "./controllers/pulse/create_pulse";
+import { retrieve_all_pulses } from "./controllers/pulse/retrieve_all_pulses";
 
 export const router = new Hono();
 
 router.post("/pulse", create_pulse);
+router.get("/pulses", retrieve_all_pulses);
