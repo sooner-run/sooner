b9e6f62b50675ef9509f206f06f96d41258f78a6
akinloluwami <akinkunmioye42@gmail.com>
2024-05-31 04:33:47 +0100

    project

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -2,9 +2,11 @@ import { Hono } from "hono";
 import { create_pulse } from "./controllers/pulse/create_pulse";
 import { retrieve_all_pulses } from "./controllers/pulse/retrieve_all_pulses";
 import { retrieve_projects } from "./controllers/projects";
+import { retrieve_single_project } from "./controllers/projects/project";
 
 export const router = new Hono();
 
 router.post("/pulses", create_pulse);
 router.get("/pulses", retrieve_all_pulses);
 router.get("/projects", retrieve_projects);
+router.get("/projects/:project", retrieve_single_project);
