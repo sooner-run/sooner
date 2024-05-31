b858215f3ee5be28ea904b5d1c6200da5c6da4b0
akinloluwami <akinkunmioye42@gmail.com>
2024-05-31 11:36:22 +0100

    request

--- sooner/src/utils/pulse.ts
+++ sooner/src/utils/pulse.ts
@@ -1,21 +1,14 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import axios from "axios";
+import request from "../configs/axios";
 
 interface Props {
-  api_key: string;
   payload: {};
 }
 
-export const sendPulse = async ({ api_key, payload }: Props) => {
-  if (api_key) {
-    try {
-      await axios.post("http://localhost:1716/v1/pulses", payload, {
-        headers: {
-          Authorization: `Bearer ${api_key}`,
-        },
-      });
-    } catch (error) {
-      console.error("Error sending pulse:", error);
-    }
+export const sendPulse = async ({ payload }: Props) => {
+  try {
+    await request.post("/pulses", payload);
+  } catch (error) {
+    console.error("Error sending pulse:", error);
   }
 };
