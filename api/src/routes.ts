5d83032e1f33e847222cb53ce257e640c05454da
akinloluwami <akinkunmioye42@gmail.com>
2024-05-31 11:13:45 +0100

    /codetime-today

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -3,6 +3,7 @@ import { create_pulse } from "./controllers/pulse/create_pulse";
 import { retrieve_all_pulses } from "./controllers/pulse/retrieve_all_pulses";
 import { retrieve_projects } from "./controllers/projects";
 import { retrieve_single_project } from "./controllers/projects/project";
+import { codetime_today } from "./controllers/codetime-today";
 
 export const router = new Hono();
 
@@ -10,3 +11,4 @@ router.post("/pulses", create_pulse);
 router.get("/pulses", retrieve_all_pulses);
 router.get("/projects", retrieve_projects);
 router.get("/projects/:project", retrieve_single_project);
+router.get("/codetime-today", codetime_today);
