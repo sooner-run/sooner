e8e833ce052d891e1cc9b7389654e4ff012825ec
akinloluwami <akinkunmioye42@gmail.com>
2024-06-03 13:33:47 +0100

    profile settings

---
+++ web/app/routes/settings.profile.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import SettingsLayout from "~/components/layout/SettingsLayout";
+
+const ProfileSettings = () => {
+  return <SettingsLayout>ProfileSettings</SettingsLayout>;
+};
+
+export default ProfileSettings;
