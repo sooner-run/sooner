0975a7057b65a3f147282d41800a043582c06885
akinloluwami <akinkunmioye42@gmail.com>
2024-06-03 10:31:22 +0100

    create insights page

---
+++ web/app/routes/insights.tsx
@@ -0,0 +1,11 @@
+import DashboardLayout from "~/components/layout/DashboardLayout";
+
+const Insights = () => {
+  return (
+    <DashboardLayout title="Insights" maintitle="Insights">
+      <div className="px-36 bg-red-500">something??/</div>
+    </DashboardLayout>
+  );
+};
+
+export default Insights;
