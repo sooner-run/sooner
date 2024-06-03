caab868d1f4a6b163d93bf36544d38417bcb5c8f
akinloluwami <akinkunmioye42@gmail.com>
2024-06-03 13:33:15 +0100

    update

--- web/app/components/layout/ProjectsLayout.tsx
+++ web/app/components/layout/ProjectsLayout.tsx
@@ -1,5 +1,6 @@
-import { useLocation } from "@remix-run/react";
+import { Link, useLocation } from "@remix-run/react";
 import React, { ReactNode } from "react";
+import { TbBolt } from "react-icons/tb";
 import DashboardLayout from "~/components/layout/DashboardLayout";
 
 const ProjectsLayout = ({ children }: { children: ReactNode }) => {
@@ -21,21 +22,48 @@ const ProjectsLayout = ({ children }: { children: ReactNode }) => {
     },
   ];
 
+  const SubLinks = () => {
+    return (
+      <div className="sticky top-0 flex flex-col w-64 px-3 border-r items-center border-l border-grey h-screen">
+        <div className="flex items-center h-16 w-full">
+          <h2 className="font-medium">Projects</h2>
+        </div>
+        <div className="w-full flex flex-col gap-y-1 text-sm">
+          <Link
+            to={"/projects"}
+            className={`px-3 border w-full py-2 ${location.pathname === "/projects" ? "bg-accent/5 rounded-full border-accent/50" : "hover:text-accent transition-colors border-transparent"}`}
+            unstable_viewTransition
+          >
+            Overview
+          </Link>
+
+          {pjs
+            .map((_) => ({
+              href: `/projects/${_.name.toLowerCase()}`,
+              title: _.name,
+            }))
+            .map((_, i) => (
+              <Link
+                unstable_viewTransition
+                key={i}
+                to={_.href}
+                className={`flex items-center gap-x-2 px-3 border w-full py-2 rounded-full ${location.pathname === _.href ? "bg-accent/5 border-accent/50" : "hover:text-accent transition-colors border-transparent"}`}
+              >
+                <TbBolt size={18} />
+                {_.title}
+              </Link>
+            ))}
+        </div>
+      </div>
+    );
+  };
+
   const location = useLocation();
   return (
     <DashboardLayout
       title="Projects"
       maintitle={location.pathname.split("/")[2] || "Projects"}
-      sublinks={{
-        index: {
-          href: "/projects",
-          title: "Overview",
-        },
-        links: pjs.map((_) => ({
-          href: `/projects/${_.name.toLowerCase()}`,
-          title: _.name,
-        })),
-      }}
+      sublinks={<SubLinks />}
     >
       <div className="w-full px-14">{children}</div>
     </DashboardLayout>
