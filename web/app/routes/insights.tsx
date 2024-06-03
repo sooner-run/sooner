b469a8835635bd324ed50f46901cac9e1d2aaa29
akinloluwami <akinkunmioye42@gmail.com>
2024-06-03 10:47:40 +0100

    added chart

--- web/app/routes/insights.tsx
+++ web/app/routes/insights.tsx
@@ -1,9 +1,29 @@
+import BarChart from "~/components/BarChart";
 import DashboardLayout from "~/components/layout/DashboardLayout";
+import Card from "~/components/ui/Card";
 
 const Insights = () => {
   return (
     <DashboardLayout title="Insights" maintitle="Insights">
-      <div className="px-36 bg-red-500">something??/</div>
+      <div className="px-36">
+        <Card className="p-4">
+          <h3 className="font-medium">Weekday average</h3>
+          <div className="my-4">
+            <BarChart
+              data={[600000, 203725, 542029, 553902, 908765, 876539, 608741]}
+              labels={[
+                "Sunday",
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+              ]}
+            />
+          </div>
+        </Card>
+      </div>
     </DashboardLayout>
   );
 };
