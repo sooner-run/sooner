34bbc6c9b769a03ca97e2ca96fd3100c437906b7
akinloluwami <akinkunmioye42@gmail.com>
2024-06-03 10:30:57 +0100

    create insights route

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -4,6 +4,7 @@ import { retrieve_all_pulses } from "./controllers/pulse/retrieve_all_pulses";
 import { retrieve_projects } from "./controllers/projects";
 import { retrieve_single_project } from "./controllers/projects/project";
 import { codetime_today } from "./controllers/codetime-today";
+import { insights } from "./controllers/insights";
 
 export const router = new Hono();
 
@@ -12,3 +13,4 @@ router.get("/pulses", retrieve_all_pulses);
 router.get("/projects", retrieve_projects);
 router.get("/projects/:project", retrieve_single_project);
 router.get("/codetime-today", codetime_today);
+router.get("/insights", insights);
