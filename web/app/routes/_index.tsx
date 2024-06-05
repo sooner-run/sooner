fcfc7e8470388dbbc06092d344693534aa0ab022
akinloluwami <akinkunmioye42@gmail.com>
2024-06-05 06:47:14 +0100

    render Screenshot component

--- web/app/routes/_index.tsx
+++ web/app/routes/_index.tsx
@@ -1,6 +1,7 @@
 import type { MetaFunction } from "@remix-run/node";
 import Hero from "~/components/landing/Hero";
 import Navbar from "~/components/landing/Navbar";
+import Screenshot from "~/components/landing/Screenshot";
 
 export const meta: MetaFunction = () => {
   return [
@@ -17,6 +18,7 @@ export default function Index() {
     <div className="text-zinc-400/80 text-sm">
       <Navbar />
       <Hero />
+      <Screenshot />
     </div>
   );
 }
