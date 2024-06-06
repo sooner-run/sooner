0a615cc7a48f0a9e0aa65797b1ffb89014c401c1
akinloluwami <akinkunmioye42@gmail.com>
2024-06-06 10:14:21 +0100

    update screenshot image

--- web/app/components/landing/Screenshot.tsx
+++ web/app/components/landing/Screenshot.tsx
@@ -5,7 +5,7 @@ const Screenshot = () => {
   return (
     <div className="relative rounded-2xl w-[90%] mx-auto my-20 shadow-2xl border border-white/5 shadow-accent/10">
       <BorderBeam />
-      <img src="/screenshot.png" className="rounded-2xl" />
+      <img src="/ss.png" className="rounded-2xl" />
       <div className="w-full h-full absolute top-0 right-0 rounded-2xl bg-gradient-to-b from-transparent to-zinc-950 to-95%"></div>
     </div>
   );
