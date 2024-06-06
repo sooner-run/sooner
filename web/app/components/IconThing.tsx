c713a0b09d6bf7eae2176cfe6b9196daaf109b0c
akinloluwami <akinkunmioye42@gmail.com>
2024-06-06 02:39:22 +0100

    accept classname

--- web/app/components/IconThing.tsx
+++ web/app/components/IconThing.tsx
@@ -1,9 +1,14 @@
 import React, { FC } from "react";
 import { IconType } from "react-icons";
 
-const IconThing: FC<{ icon: IconType }> = ({ icon }) => {
+const IconThing: FC<{ icon: IconType; className?: string }> = ({
+  icon,
+  className,
+}) => {
   return (
-    <div className="size-10 bg-grey-300 rounded-2xl flex items-center justify-center">
+    <div
+      className={`size-10 bg-grey-300 rounded-2xl flex items-center justify-center ${className}`}
+    >
       {icon({ size: 20 })}
     </div>
   );
