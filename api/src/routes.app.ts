884ede161d4e5815931cee97da72a0bb21cf33c2
akinloluwami <akinkunmioye42@gmail.com>
2024-06-07 09:53:19 +0100

    get profile

--- api/src/routes.app.ts
+++ api/src/routes.app.ts
@@ -1,5 +1,7 @@
 import { Hono } from "hono";
 import { api_key } from "./controllers/app/api_key";
+import { profile } from "./controllers/app/profile";
 export const router = new Hono();
 
 router.get("/api-key", api_key);
+router.get("/profile", profile);
