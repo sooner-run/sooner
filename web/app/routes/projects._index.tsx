6a428ae4eaa008419eae71d01b8fce9f60c5360b
akinloluwami <akinkunmioye42@gmail.com>
2024-06-07 12:00:31 +0100

    ^

--- web/app/routes/projects._index.tsx
+++ web/app/routes/projects._index.tsx
@@ -1,44 +1,24 @@
-import { faker } from "@faker-js/faker";
-import React, { useEffect, useState } from "react";
+import { LoaderFunctionArgs } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 import ProjectCard from "~/components/ProjectCard";
 import ProjectsLayout from "~/components/layout/ProjectsLayout";
+import { fetchLoader } from "~/utils/loader";
+
+export async function loader(args: LoaderFunctionArgs) {
+  return fetchLoader(args, "/v1/projects");
+}
 
 const Projects = () => {
-  const [data, setData] = useState<
-    { name: string; time: string; language: string }[]
-  >([]);
-
-  useEffect(() => {
-    const projects = Array.from({
-      length: faker.number.int({ min: 4, max: 22 }),
-    }).map(() => ({
-      name: faker.internet.domainWord(),
-      time: `${faker.number.int({ min: 1, max: 16 })}h ${faker.number.int({ min: 1, max: 59 })}m ${faker.number.int({ min: 16, max: 59 })}s`,
-      language: faker.helpers.arrayElement([
-        "typescript",
-        "css",
-        "go",
-        "rust",
-        "javascript",
-        "c++",
-        "php",
-        "java",
-        "python",
-        "erlang",
-        "html",
-        "scss",
-      ]),
-    }));
-
-    setData(projects);
-  }, []);
+  const data = useLoaderData<typeof loader>();
 
   return (
     <ProjectsLayout>
       <div className="grid grid-cols-3 gap-4">
-        {data.map((project, i) => (
+        {JSON.stringify(data)}
+
+        {/* {data.map((project: any, i: number) => (
           <ProjectCard key={i} {...project} />
-        ))}
+        ))} */}
       </div>
     </ProjectsLayout>
   );
