1b76a19d042ebf45bff3289a77ef03a0730a9ff0
akinloluwami <akinkunmioye42@gmail.com>
2024-06-07 11:58:38 +0100

    stats

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -5,6 +5,7 @@ import { retrieve_projects } from "./controllers/projects";
 import { retrieve_single_project } from "./controllers/projects/project";
 import { codetime_today } from "./controllers/codetime-today";
 import { insights } from "./controllers/insights";
+import { stats } from "./controllers/stats";
 
 export const router = new Hono();
 
@@ -14,3 +15,4 @@ router.get("/projects", retrieve_projects);
 router.get("/projects/:project", retrieve_single_project);
 router.get("/codetime-today", codetime_today);
 router.get("/insights", insights);
+router.get("/stats", stats);
