c344cda1c685dd49b1c2b8781b96e98e905ef0f4
akinloluwami <akinkunmioye42@gmail.com>
2024-06-07 14:40:45 +0100

    set cookie

--- api/src/controllers/auth/login.ts
+++ api/src/controllers/auth/login.ts
@@ -4,6 +4,9 @@ import { users } from "../../db/schema";
 import { compareSync } from "bcryptjs";
 import { eq } from "drizzle-orm";
 import { sign } from "jsonwebtoken";
+import { setCookie } from "hono/cookie";
+import dayjs from "dayjs";
+import { isProd } from "../../../constants";
 
 export const login = async (c: Context) => {
   const { email, password }: { email: string; password: string } =
@@ -26,7 +29,15 @@ export const login = async (c: Context) => {
 
     const token = sign({ id: user.id }, process.env.JWT_SECRET!);
 
-    return c.json({ message: "Logged in", token }, 200);
+    setCookie(c, "sooner.auth-token", token, {
+      domain: "localhost",
+      secure: isProd,
+      sameSite: "strict",
+      expires: dayjs().add(90, "days").toDate(),
+      httpOnly: true,
+    });
+
+    return c.json({ message: "Logged in" }, 200);
   } catch (error) {
     console.log(error);
     return c.json({ message: "Something went wrong." }, 500);
