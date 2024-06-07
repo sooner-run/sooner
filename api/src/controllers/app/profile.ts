3da98b67019416f02a4f90a10e50bb2d75bf3d8b
akinloluwami <akinkunmioye42@gmail.com>
2024-06-07 09:53:45 +0100

    create profile controller

---
+++ api/src/controllers/app/profile.ts
@@ -0,0 +1,19 @@
+import { Context } from "hono";
+import { db } from "../../db";
+import { users } from "../../db/schema";
+import { eq } from "drizzle-orm";
+
+export const profile = async (c: Context) => {
+  try {
+    const [user] = await db
+      .select({
+        username: users.username,
+        email: users.email,
+      })
+      .from(users)
+      .where(eq(users.id, c.get("auth.user_id")));
+    return c.json(user, 200);
+  } catch (error) {
+    return c.json({ message: "Something went wrong." }, 500);
+  }
+};
