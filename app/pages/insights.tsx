3a953bc4706643d13a4b0b3d785aaa0e1d1d35d4
akinloluwami <akinkunmioye42@gmail.com>
2024-06-08 06:28:27 +0100

    replace mock data with real data

--- app/pages/insights.tsx
+++ app/pages/insights.tsx
@@ -7,67 +7,33 @@ import DashboardLayout from "@/components/layout/DashboardLayout";
 import Card from "@/components/ui/Card";
 import { getColorForLanguage } from "@/utils/getColorForLanguage";
 import { time_to_human } from "@/utils/time_to_human";
+import useSWR from "swr";
+import { fetcher } from "@/utils/fetcher";
+import { InsightsData } from "./types";
 
 const Insights = () => {
-  const [languages, setLanguages] = useState<
-    {
-      time: number;
-      language: string;
-    }[]
-  >([]);
+  const { data, isLoading } = useSWR<InsightsData>("/v1/insights", fetcher);
 
-  const [projects, setProjects] = useState<{ time: number; project: string }[]>(
-    []
-  );
+  if (!data) return <>Loading...</>;
 
-  useEffect(() => {
-    setLanguages(
-      Array.from({ length: 8 }).map(() => ({
-        language: faker.helpers.arrayElement([
-          "HTML",
-          "CSS",
-          "JavaScript",
-          "PHP",
-          "Typescript",
-          "SQL",
-        ]),
-        time: faker.number.int({ min: 1_000_000, max: 5_000_000 }),
-      }))
-    );
-
-    setProjects(
-      Array.from({ length: 8 }).map(() => ({
-        project: faker.internet.domainWord(),
-        time: faker.number.int({ min: 1_000_000, max: 5_000_000 }),
-      }))
-    );
-  }, []);
+  const weekdayAverage = data.weekday_average;
+  const times = weekdayAverage.map((_) => _.time);
+  const labels = weekdayAverage.map((_) => _.day);
 
   return (
     <DashboardLayout title="Insights" maintitle="Insights">
-      <div className="px-52 flex flex-col gap-y-5">
+      <div className="lg:px-52 px-5 flex flex-col gap-y-5">
         <Card className="p-4">
           <h3 className="font-medium text-grey-100">Weekday average</h3>
           <div className="my-4">
-            <BarChart
-              data={[600000, 203725, 542029, 553902, 908765, 876539, 608741]}
-              labels={[
-                "Sunday",
-                "Monday",
-                "Tuesday",
-                "Wednesday",
-                "Thursday",
-                "Friday",
-                "Saturday",
-              ]}
-            />
+            <BarChart data={times} labels={labels} />
           </div>
         </Card>
 
-        <div className="flex gap-x-5">
-          <Card className="p-4">
+        <div className="flex gap-5 lg:flex-nowrap flex-wrap">
+          <Card className="p-4 lg:w-1/2">
             <h3 className="mb-4 text-grey-100 font-medium">Top languages</h3>
-            {languages
+            {data.top_languages
               .sort((a, b) => b.time - a.time)
               .map((l, i) => (
                 <div
@@ -82,16 +48,16 @@ const Insights = () => {
 
                     {l.language}
                   </p>
-                  <p className="flex items-center gap-x-3">
+                  <p className="flex items-center gap-x-3 text-xs">
                     {time_to_human(l.time)}
                     <Clock01Icon size={20} />
                   </p>
                 </div>
               ))}
           </Card>
-          <Card className="p-4">
+          <Card className="p-4 lg:w-1/2">
             <h3 className="mb-4 text-grey-100 font-medium">Top projects</h3>
-            {projects
+            {data.top_projects
               .sort((a, b) => b.time - a.time)
               .map((p, i) => (
                 <div
@@ -99,7 +65,7 @@ const Insights = () => {
                   key={i}
                 >
                   <p>{p.project}</p>
-                  <p className="flex items-center gap-x-3">
+                  <p className="flex items-center gap-x-3 text-xs">
                     {time_to_human(p.time)}
                     <Clock01Icon size={20} />
                   </p>
