b3abc3d74463dfe224d22eaceeeff8651d2e478d
akinloluwami <akinkunmioye42@gmail.com>
2024-06-08 14:16:40 +0100

    responsive

--- app/components/landing/Navbar.tsx
+++ app/components/landing/Navbar.tsx
@@ -22,9 +22,11 @@ const Navbar = () => {
   ];
 
   return (
-    <div className="flex items-center justify-between py-5 px-20 sticky z-30 top-0 backdrop-blur-lg">
-      <img src="/logo.png" alt="Sooner logo" className="w-28" />
-      <div className="flex items-center gap-x-2 border border-zinc-300/10 p-1 rounded-full backdrop-blur-3xl">
+    <div className="flex items-center justify-between py-5 lg:px-20 px-5 sticky z-30 top-0 backdrop-blur-lg">
+      <Link href="/">
+        <img src="/logo.png" alt="Sooner logo" className="w-28" />
+      </Link>
+      <div className="lg:flex hidden items-center gap-x-2 border border-zinc-300/10 p-1 rounded-full backdrop-blur-3xl">
         {links.map((link) => (
           <Link
             href={link.href}
@@ -35,12 +37,13 @@ const Navbar = () => {
           </Link>
         ))}
       </div>
-      <div className="flex font-medium gap-x-7 text-zinc-300 items-center">
+      <div className="lg:flex hidden font-medium gap-x-7 text-zinc-300 items-center">
         <Link href="/login">Login</Link>
         <ShimmerButton className="!text-zinc-300 !py-3">
           Get started
         </ShimmerButton>
       </div>
+
       <div className="h-[1px] w-full bg-gradient-to-r absolute right-0 bottom-0 from-transparent via-accent/20 to-transparent"></div>
     </div>
   );
