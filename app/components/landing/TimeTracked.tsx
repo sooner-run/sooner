956a805e37841def9c66d8f70c163b14fe660a18
akinloluwami <akinkunmioye42@gmail.com>
2024-06-08 14:17:21 +0100

    responsive

--- app/components/landing/TimeTracked.tsx
+++ app/components/landing/TimeTracked.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const TimeTracked = () => {
   return (
-    <div className="px-20 py-32">
-      <h1 className="text-center text-[300px] font-semibold opacity-50">
+    <div className="lg:px-20 px-5 py-32">
+      <h1 className="text-center lg:text-[300px] text-9xl font-semibold opacity-50">
         4,200
       </h1>
-      <p className="text-center font-medium text-5xl text-white/90">
+      <p className="text-center font-medium lg:text-5xl text-3xl text-white/90">
         Minutes tracked
       </p>
     </div>
