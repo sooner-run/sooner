e09897cf1ec6752516b96bf2eb83dba72f2549de
akinloluwami <akinkunmioye42@gmail.com>
2024-06-08 14:13:32 +0100

    order

--- api/src/controllers/pulse/retrieve_all_pulses.ts
+++ api/src/controllers/pulse/retrieve_all_pulses.ts
@@ -1,14 +1,15 @@
 import { Context } from "hono";
 import { db } from "../../db";
 import { pulses } from "../../db/schema";
-import { eq } from "drizzle-orm";
+import { desc, eq } from "drizzle-orm";
 
 export const retrieve_all_pulses = async (c: Context) => {
   try {
     const _pulses = await db
       .select()
       .from(pulses)
-      .where(eq(pulses.user_id, c.get("user_id")));
+      .where(eq(pulses.user_id, c.get("user_id")))
+      .orderBy(desc(pulses.created_at));
 
     return c.json({ pulses: _pulses }, 200);
   } catch (error) {
