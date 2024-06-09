5282a0b96c5007803c819db821a301077d5816f6
akinloluwami <akinkunmioye42@gmail.com>
2024-06-09 02:48:37 +0100

    tweak MainLayout transition

--- app/components/layout/MainLayout.tsx
+++ app/components/layout/MainLayout.tsx
@@ -3,8 +3,8 @@ import { motion } from "framer-motion";
 
 const variants = {
   hidden: { opacity: 0 },
-  enter: { opacity: 1, transition: { duration: 0.5 } },
-  exit: { opacity: 0.1, transition: { duration: 0.5 } },
+  enter: { opacity: 1, transition: { duration: 0.3 } },
+  exit: { opacity: 0, transition: { duration: 0.3 } },
 };
 
 interface MainLayoutProps {
