0fe320e2649f64d75d4811157c523f50ebf57612
akinloluwami <akinkunmioye42@gmail.com>
2024-06-09 10:33:19 +0100

    rename

---
+++ api/src/controllers/calculateStreak.ts
@@ -0,0 +1,33 @@
+import { db } from "../db";
+import { and, eq, gte, lte, sum } from "drizzle-orm";
+import { pulses } from "../db/schema";
+import dayjs from "dayjs";
+
+export const calculateStreak = async (userId: string): Promise<number> => {
+  let streak = 0;
+  let currentDay = dayjs().startOf("day");
+
+  while (true) {
+    const [dayPulse] = await db
+      .select({
+        time: sum(pulses.time),
+      })
+      .from(pulses)
+      .where(
+        and(
+          eq(pulses.user_id, userId),
+          gte(pulses.created_at, currentDay.toDate()),
+          lte(pulses.created_at, currentDay.endOf("day").toDate())
+        )
+      );
+
+    if (Number(dayPulse.time) > 0) {
+      streak++;
+      currentDay = currentDay.subtract(1, "day");
+    } else {
+      break;
+    }
+  }
+
+  return streak;
+};
