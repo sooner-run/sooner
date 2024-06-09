5c90ec1753e95db74de6ea766ae57068e459c03f
akinloluwami <akinkunmioye42@gmail.com>
2024-06-09 09:04:27 +0100

    kiss

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,39 +1,11 @@
-import { useEffect } from "react";
-import { useRouter } from "next/router";
 import { AppProps } from "next/app";
-import { AnimatePresence } from "framer-motion";
-import MainLayout from "@/components/layout/MainLayout";
-import DashboardLayout from "@/components/layout/DashboardLayout";
 import "@/styles/globals.css";
+import { AnimatePresence } from "framer-motion";
 
-export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = () => {
-      window.scrollTo(0, 0);
-    };
-
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
-
-  const dashboardRoutes = ["/dashboard", "/projects", "/settings", "/insights"];
-  const isDashboardRoute = dashboardRoutes.includes(router.pathname);
-
+export default function App({ Component, pageProps, router }: AppProps) {
   return (
     <AnimatePresence mode="wait">
-      {isDashboardRoute ? (
-        <DashboardLayout key={router.route}>
-          <Component {...pageProps} />
-        </DashboardLayout>
-      ) : (
-        <MainLayout key={router.route}>
-          <Component {...pageProps} />
-        </MainLayout>
-      )}
+      <Component {...pageProps} key={router.route} />
     </AnimatePresence>
   );
 }
