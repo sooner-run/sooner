ba81fe5aefb1cb08c4bdd302f6e3d651d9d86da7
akinloluwami <akinkunmioye42@gmail.com>
2024-06-10 03:39:26 +0100

    ??

--- api/src/controllers/pulse/createPulse.ts
+++ api/src/controllers/pulse/createPulse.ts
@@ -2,6 +2,7 @@ import { Context } from "hono";
 import { pulses } from "../../db/schema";
 import { db } from "../../db";
 import languageData from "../../../data/languages.json";
+import * as path from "path";
 
 const getLanguageFromPath = (path: string) => {
   const extension = `.${path.split(".").pop()}`;
