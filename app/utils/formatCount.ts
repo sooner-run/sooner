77fbecafc38be19ceacf6111def6888d83c06bc7
akinloluwami <akinkunmioye42@gmail.com>
2024-06-10 03:42:35 +0100

    ...

--- app/utils/formatCount.ts
+++ app/utils/formatCount.ts
@@ -1,14 +1,29 @@
-export const formatCount = (count: number): string => {
+export const formatCount = (
+  count: number,
+  abbreviated: boolean = false
+): string => {
   const hours = Math.floor(count / (1000 * 60 * 60));
   const minutes = Math.floor((count % (1000 * 60 * 60)) / (1000 * 60));
 
   if (hours === 0 && minutes === 0) {
     return "0 minutes";
-  } else if (hours === 0) {
-    return `${minutes} minutes`;
-  } else if (minutes === 0) {
-    return `${hours} hours`;
+  }
+
+  if (abbreviated) {
+    if (hours === 0) {
+      return `${minutes}m`;
+    } else if (minutes === 0) {
+      return `${hours}h`;
+    } else {
+      return `${hours}h ${minutes}m`;
+    }
   } else {
-    return `${hours} hours ${minutes} minutes`;
+    if (hours === 0) {
+      return `${minutes} minutes`;
+    } else if (minutes === 0) {
+      return `${hours} hours`;
+    } else {
+      return `${hours} hours ${minutes} minutes`;
+    }
   }
 };
