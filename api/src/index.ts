4c300f16fd41e5117cf3c043a7d6208c1987db0e
akinloluwami <akinkunmioye42@gmail.com>
2024-06-10 21:38:17 +0100

    update cors policy

--- api/src/index.ts
+++ api/src/index.ts
@@ -14,7 +14,15 @@ env.config();
 const app = new Hono();
 
 app.use("*", prettyJSON());
-app.use("*", cors({ origin: process.env.FRONTEND_DOMAIN!, credentials: true }));
+app.use(
+  "*",
+  cors({
+    origin: process.env.FRONTEND_DOMAIN!,
+    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 //** Would come back to this when API access is shipped. **/
 // app.use("/v1/*", cors({ origin: "*" }));
