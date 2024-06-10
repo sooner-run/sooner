612e33c12ce6a9b228533102036ea5351fda2a5c
akinloluwami <akinkunmioye42@gmail.com>
2024-06-10 03:37:43 +0100

    tz

--- api/src/controllers/calculateStreak.ts
+++ api/src/controllers/calculateStreak.ts
@@ -2,32 +2,65 @@ import { db } from "../db";
 import { and, eq, gte, lte, sum } from "drizzle-orm";
 import { pulses } from "../db/schema";
 import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+const timeZone = "Africa/Lagos";
 
 export const CalculateStreak = async (userId: string): Promise<number> => {
-  let streak = 0;
-  let currentDay = dayjs().startOf("day");
+  try {
+    let streak = 0;
+    let currentDay = dayjs().tz(timeZone).startOf("day");
 
-  while (true) {
-    const [dayPulse] = await db
-      .select({
-        time: sum(pulses.time),
-      })
-      .from(pulses)
-      .where(
-        and(
-          eq(pulses.user_id, userId),
-          gte(pulses.created_at, currentDay.toDate()),
-          lte(pulses.created_at, currentDay.endOf("day").toDate())
-        )
-      );
+    while (true) {
+      const startOfDay = currentDay
+        .startOf("day")
+        .add(1, "hour")
+        .tz(timeZone, true)
+        .toDate();
+      const endOfDay = currentDay
+        .endOf("day")
+        .add(1, "hour")
+        .tz(timeZone, true)
+        .toDate();
 
-    if (Number(dayPulse.time) > 0) {
-      streak++;
-      currentDay = currentDay.subtract(1, "day");
-    } else {
-      break;
+      console.log("====================================");
+      console.log({
+        currentDay: currentDay.format(),
+        startOfDay,
+        endOfDay,
+      });
+      console.log("====================================");
+
+      const [dayPulse] = await db
+        .select({
+          time: sum(pulses.time),
+        })
+        .from(pulses)
+        .where(
+          and(
+            eq(pulses.user_id, userId),
+            gte(pulses.created_at, startOfDay),
+            lte(pulses.created_at, endOfDay)
+          )
+        );
+
+      console.log(`Day Pulse Time: ${Number(dayPulse.time)}`);
+
+      if (Number(dayPulse.time) > 0) {
+        streak++;
+        currentDay = currentDay.subtract(1, "day");
+      } else {
+        break;
+      }
     }
-  }
 
-  return streak;
+    return streak;
+  } catch (error) {
+    console.error("Error calculating streak:", error);
+    return 0;
+  }
 };
