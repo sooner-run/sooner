bb2ba530d9e75c7cbdd113347666ea91ffb784f6
akinloluwami <akinkunmioye42@gmail.com>
2024-06-10 04:06:13 +0100

    /logout

--- api/src/routes.auth.ts
+++ api/src/routes.auth.ts
@@ -1,8 +1,10 @@
 import { Hono } from "hono";
 import { Signup } from "./controllers/auth/signup";
 import { Login } from "./controllers/auth/login";
+import { Logout } from "./controllers/auth/logout";
 
 export const router = new Hono();
 
 router.post("/signup", Signup);
 router.post("/login", Login);
+router.post("/logout", Logout);
