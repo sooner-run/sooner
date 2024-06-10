505b14c60324a0091da5603c36d8b1fc22e65264
akinloluwami <akinkunmioye42@gmail.com>
2024-06-10 03:42:14 +0100

    add loading prop

--- app/pages/projects/[project].tsx
+++ app/pages/projects/[project].tsx
@@ -1,6 +1,6 @@
 import { faker } from "@faker-js/faker";
 import { ArrowDown01Icon, Clock01Icon } from "hugeicons-react";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { GoDotFill } from "react-icons/go";
 import DonutChart from "@/components/DonutChart";
 import LineChart from "@/components/LineChart";
@@ -9,57 +9,34 @@ import Card from "@/components/ui/Card";
 import { getColorForLanguage } from "@/utils/getColorForLanguage";
 import { time_to_human } from "@/utils/time_to_human";
 import DotPattern from "@/components/magicui/dot-pattern";
+import { useRouter } from "next/router";
+import useSWR from "swr";
+import { fetcher } from "@/utils/fetcher";
+import { ProjectData } from "@/types";
+import dayjs from "dayjs";
+import { truncate } from "@/utils/truncate";
 
 const Project = () => {
-  const [language, setLanguage] = useState("");
-
-  const [files, setFiles] = useState<
-    {
-      time: number;
-      file: string;
-    }[]
-  >([]);
-  const [branches, setBranches] = useState<
-    {
-      branch: string;
-      time: number;
-    }[]
-  >([]);
+  const router = useRouter();
+  const [url, setUrl] = useState("");
 
   useEffect(() => {
-    setLanguage(
-      faker.helpers.arrayElement([
-        "typescript",
-        "css",
-        "go",
-        "rust",
-        "javascript",
-        "c++",
-        "php",
-        "java",
-        "python",
-        "erlang",
-        "html",
-        "scss",
-      ])
-    );
-    setFiles(
-      Array.from({ length: 43 }).map(() => ({
-        file: faker.system.fileName(),
-        time: faker.number.int({ min: 600_000, max: 100_000_000 }),
-      }))
-    );
+    if (router.asPath) {
+      const projectId = router.asPath.split("/").pop();
+      if (projectId !== "[project]") {
+        const startDate = dayjs().subtract(6, "days").toISOString();
+        const endDate = dayjs().toISOString();
+        setUrl(
+          `/v1/projects/${projectId}?start_date=${startDate}&end_date=${endDate}`
+        );
+      }
+    }
+  }, [router.asPath]);
 
-    setBranches(
-      Array.from({ length: 10 }).map(() => ({
-        branch: faker.git.branch(),
-        time: faker.number.int({ min: 600_000, max: 100_000_000 }),
-      }))
-    );
-  }, []);
+  const { data, error, isLoading } = useSWR<ProjectData>(url, fetcher);
 
   return (
-    <ProjectsLayout>
+    <ProjectsLayout loading={isLoading || !data}>
       <div className="flex flex-col gap-y-5">
         <Card className="p-4 shrink-0 divide-x divide-grey grid grid-cols-2 items-center gap-x-5 gap-y-7 lg:grid-cols-4 first:pl-0 last:pr-0">
           <div className="px-2">
@@ -72,7 +49,7 @@ const Project = () => {
                 />
               </div>
               <p className="font-medium flex items-center gap-x-2 text-sm mt-3">
-                88h 23m 11s
+                {data?.time_human_readable}
               </p>
             </div>
           </div>
@@ -96,8 +73,11 @@ const Project = () => {
                 <p className="text-sm font-semibold">Top language</p>
               </div>
               <div className="flex items-center gap-x-1 mt-3">
-                <GoDotFill color={getColorForLanguage(language)!} size={30} />
-                <p className="text-sm">{language}</p>
+                <GoDotFill
+                  color={getColorForLanguage(data?.top_language!)!}
+                  size={30}
+                />
+                <p className="text-sm">{data?.top_language}</p>
               </div>
             </div>
           </div>
@@ -107,7 +87,7 @@ const Project = () => {
                 <p className="text-sm font-semibold">All time</p>
               </div>
               <p className="font-medium flex items-center gap-x-2 text-sm mt-3">
-                201h 54m 23s
+                {data?.all_time}
               </p>
             </div>
           </div>
@@ -115,16 +95,8 @@ const Project = () => {
         <Card className="relative p-4">
           <DotPattern className="rounded-2xl opacity-15 p-2 pb-7 pl-12" />
           <LineChart
-            labels={[
-              "May 22nd",
-              "May 23rd",
-              "May 24th",
-              "May 25th",
-              "May 26th",
-              "May 27th",
-              "May 28th",
-            ]}
-            data={[4, 10, 7, 8, 14, 8, 9]}
+            labels={data?.timeseries.map((t) => t.date)!}
+            data={data?.timeseries.map((t) => t.time)!}
           />
         </Card>
         <div className="flex gap-x-4">
@@ -132,68 +104,37 @@ const Project = () => {
             <h3>Top languages</h3>
             <div className="p-10">
               <DonutChart
-                labels={[
-                  "typescript",
-                  "python",
-                  "kotlin",
-                  "css",
-                  "javascript",
-                  "elixir",
-                  "html",
-                ]}
-                data={[40670, 69372, 293624, 29393, 202820, 194760]}
+                labels={data?.languages.map((l) => l.language)!}
+                data={data?.languages.map((l) => l.time)!}
               />
             </div>
           </Card>
-          <Card className="p-4">
-            <DonutChart
-              labels={[
-                "May 22nd",
-                "May 23rd",
-                "May 24th",
-                "May 25th",
-                "May 26th",
-                "May 27th",
-                "May 28th",
-              ]}
-              data={[4, 10, 7, 8, 14, 8, 9]}
-            />
-          </Card>
+          <Card className="p-4">...</Card>
         </div>
         <div className="flex items-start gap-x-4">
           <Card className="p-4">
             <h3 className="mb-4 text-grey-100 font-medium">Files</h3>
-            {files
-              .sort((a, b) => b.time - a.time)
-              .map((f, i) => (
-                <div
-                  className="flex justify-between w-full my-4 text-sm"
-                  key={i}
-                >
-                  <p>{f.file}</p>
-                  <p className="flex items-center gap-x-3">
-                    {time_to_human(f.time)}
-                    <Clock01Icon size={20} />
-                  </p>
-                </div>
-              ))}
+            {data?.files.map((f, i) => (
+              <div className="flex justify-between w-full my-4 text-sm" key={i}>
+                <p>{truncate(f.file, 30)}</p>
+                <p className="flex items-center gap-x-3">
+                  {time_to_human(f.time)}
+                  <Clock01Icon size={20} />
+                </p>
+              </div>
+            ))}
           </Card>
           <Card className="p-4">
             <h3 className="mb-4 text-grey-100 font-medium">Branches</h3>
-            {branches
-              .sort((a, b) => b.time - a.time)
-              .map((b, i) => (
-                <div
-                  className="flex justify-between w-full my-4 text-sm"
-                  key={i}
-                >
-                  <p>{b.branch}</p>
-                  <p className="flex items-center gap-x-3">
-                    {time_to_human(b.time)}
-                    <Clock01Icon size={20} />
-                  </p>
-                </div>
-              ))}
+            {data?.branches.map((b, i) => (
+              <div className="flex justify-between w-full my-4 text-sm" key={i}>
+                <p>{b.branch}</p>
+                <p className="flex items-center gap-x-3">
+                  {time_to_human(b.time)}
+                  <Clock01Icon size={20} />
+                </p>
+              </div>
+            ))}
           </Card>
         </div>
       </div>
