40471542137c2f448865afa2356a05f34a78f5e9
akinloluwami <akinkunmioye42@gmail.com>
2024-06-10 22:23:08 +0100

    create middlware to protect routes

---
+++ app/middleware.ts
@@ -0,0 +1,38 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const middleware = async (req: NextRequest) => {
+  const token = req.cookies.get("sooner.auth-token")?.value;
+
+  if (!token) {
+    const allowedPaths = [
+      "/login",
+      "/",
+      "/docs",
+      "/pricing",
+      "/promise",
+      "/faq",
+      "/changelog",
+      "/signup",
+    ];
+
+    if (!allowedPaths.includes(req.nextUrl.pathname)) {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
+  }
+
+  if (token) {
+    if (req.url.includes("/login"))
+      return NextResponse.redirect(new URL("/dashboard", req.url));
+
+    if (req.url.includes("/signup"))
+      return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
+};
+
+export const config = {
+  matcher: [
+    "/((?!api/|_next/|_proxy/|_static|_vercel|favicon.ico|sitemap.xml|robots.txt|logo.svg|grid-bg.png).*)",
+  ],
+};
+
+export default middleware;
