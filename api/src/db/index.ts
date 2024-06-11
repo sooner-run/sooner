4a2170392617426da197692990304f4ce4283a8d
akinloluwami <akinkunmioye42@gmail.com>
2024-06-11 17:49:22 +0100

    fixed db ternimated shit for real

--- api/src/db/index.ts
+++ api/src/db/index.ts
@@ -1,6 +1,9 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Client } from "pg";
 import * as schema from "./schema";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const client = new Client(process.env.DB_URL);
 
