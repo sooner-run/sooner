421939ea0720e620aeaa2ce2d3a8017adb277a47
akinloluwami <akinkunmioye42@gmail.com>
2024-06-11 17:51:30 +0100

    removed timezone stuff (it doesn't work anyway)

--- api/src/controllers/calculateStreak.ts
+++ api/src/controllers/calculateStreak.ts
@@ -2,30 +2,15 @@ import { db } from "../db";
 import { and, eq, gte, lte, sum } from "drizzle-orm";
 import { pulses } from "../db/schema";
 import dayjs from "dayjs";
-import utc from "dayjs/plugin/utc";
-import timezone from "dayjs/plugin/timezone";
-
-dayjs.extend(utc);
-dayjs.extend(timezone);
-
-const timeZone = "Africa/Lagos";
 
 export const CalculateStreak = async (userId: string): Promise<number> => {
   try {
     let streak = 0;
-    let currentDay = dayjs().tz(timeZone).startOf("day");
+    let currentDay = dayjs().startOf("day");
 
     while (true) {
-      const startOfDay = currentDay
-        .startOf("day")
-        .add(1, "hour")
-        .tz(timeZone, true)
-        .toDate();
-      const endOfDay = currentDay
-        .endOf("day")
-        .add(1, "hour")
-        .tz(timeZone, true)
-        .toDate();
+      const startOfDay = currentDay.startOf("day").add(1, "hour").toDate();
+      const endOfDay = currentDay.endOf("day").add(1, "hour").toDate();
 
       const [dayPulse] = await db
         .select({
