93cae2481e60c53c5ea0112c847a5faf24a40505
akinloluwami <akinkunmioye42@gmail.com>
2024-06-11 14:37:04 +0100

    fixed terminated shit

--- api/src/db/index.ts
+++ api/src/db/index.ts
@@ -8,18 +8,9 @@ const client = new Client({
   connectionString: process.env.DB_URL,
 });
 
-const ConnectClient = async () => {
-  await client
-    .connect()
-    .then(() => {
-      console.log("DB Connected!");
-    })
-    .catch(() => {
-      console.log("Fialed to connect.");
-      console.log(process.env.DB_URL);
-    });
-};
-
-ConnectClient();
+client.connect(function (err) {
+  if (err) console.log(err);
+  else console.log("Connected!");
+});
 
 export const db = drizzle(client);
