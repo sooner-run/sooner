16ad0748a101893ee0b3f28c468f93bc628d0ae5
akinloluwami <akinkunmioye42@gmail.com>
2024-06-11 22:47:19 +0100

    pool connection to db

--- api/src/db/index.ts
+++ api/src/db/index.ts
@@ -1,15 +1,13 @@
 import { drizzle } from "drizzle-orm/node-postgres";
-import { Client } from "pg";
+
 import * as schema from "./schema";
 import dotenv from "dotenv";
+import { Pool } from "pg";
 
 dotenv.config();
 
-const client = new Client(process.env.DB_URL);
-
-client.connect(function (err) {
-  if (err) console.log(err);
-  else console.log("Connected!");
+const pool = new Pool({
+  connectionString: process.env.DB_URL,
 });
 
-export const db = drizzle(client, { schema });
+export const db = drizzle(pool, { schema });
