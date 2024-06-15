2616e71bd5ea9d32d488ce42eeb59190600b27cb
akinloluwami <akinkunmioye42@gmail.com>
2024-06-15 17:55:18 +0100

    create onboarding page

---
+++ app/pages/onboarding.tsx
@@ -0,0 +1,12 @@
+import DashboardLayout from "@/components/layout/DashboardLayout";
+import React from "react";
+
+const Onboarding = () => {
+  return (
+    <DashboardLayout title="Onboarding" maintitle="Get started with Sooner">
+      Onboarding
+    </DashboardLayout>
+  );
+};
+
+export default Onboarding;
