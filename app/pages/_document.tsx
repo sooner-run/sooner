8201069b938a0848d277c4a1ab3485f8e9b0908b
akinloluwami <akinkunmioye42@gmail.com>
2024-06-15 03:07:37 +0100

    add Pirsch Analytics

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          defer
+          src="https://api.pirsch.io/pa.js"
+          id="pianjs"
+          data-code="1JAjtFvaT5tJG281MVVi2myuYWfb7Ovx"
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
