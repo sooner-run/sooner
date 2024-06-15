45dc4c144f6b0e83efd729a633bfbeabde308ea6
akinloluwami <akinkunmioye42@gmail.com>
2024-06-15 17:52:27 +0100

    /verify

--- api/src/routes.auth.ts
+++ api/src/routes.auth.ts
@@ -2,9 +2,11 @@ import { Hono } from "hono";
 import { Signup } from "./controllers/auth/signup";
 import { Login } from "./controllers/auth/login";
 import { Logout } from "./controllers/auth/logout";
+import { Verify } from "./controllers/auth/verify";
 
 export const router = new Hono();
 
 router.post("/signup", Signup);
 router.post("/login", Login);
 router.post("/logout", Logout);
+router.post("/verify", Verify);
