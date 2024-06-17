b1f488de752eacd7b09b8685f78b53096ced228b
akinloluwami <akinkunmioye42@gmail.com>
2024-06-17 23:28:13 +0100

    set up logsnag

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,11 +1,17 @@
 import { AppProps } from "next/app";
 import "@/styles/globals.css";
 import { AnimatePresence } from "framer-motion";
+import { LogSnagProvider } from "@logsnag/next";
 
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
-    <AnimatePresence mode="wait">
-      <Component {...pageProps} key={router.route} />
-    </AnimatePresence>
+    <LogSnagProvider
+      token={process.env.NEXT_PUBLIC_LOGSNAG_TOKEN!}
+      project="sooner"
+    >
+      <AnimatePresence mode="wait">
+        <Component {...pageProps} key={router.route} />
+      </AnimatePresence>
+    </LogSnagProvider>
   );
 }
