26ff523380015626c661ddacec264d773674f35b
akinloluwami <akinkunmioye42@gmail.com>
2024-06-17 18:04:36 +0100

    add head

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -7,17 +7,25 @@ import OpenSource from "@/components/landing/OpenSource";
 import Screenshot from "@/components/landing/Screenshot";
 import TeamFeatures from "@/components/landing/TeamFeatures";
 import TimeTracked from "@/components/landing/TimeTracked";
+import Head from "next/head";
 
 export default function Index() {
   return (
     <div className="text-zinc-400/80 text-sm">
+      <Head>
+        <title>Sooner ~ Time tracking for devs and software teams.</title>
+        <meta
+          name="description"
+          content="Track your coding time efficiently with Sooner, the best time tracking tool for developers and software teams."
+        />
+      </Head>
       <Navbar />
       <Hero />
       <Screenshot />
-      <TimeTracked />
-      <Features />
-      <ActivityChart />
-      <TeamFeatures />
+      {/* <TimeTracked /> */}
+      {/* <Features /> */}
+      {/* <ActivityChart /> */}
+      {/* <TeamFeatures /> */}
       <OpenSource />
       <Footer />
     </div>
