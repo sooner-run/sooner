556cbe1d686a0a215b7705c6789ac0158e95b7cc
akinloluwami <akinkunmioye42@gmail.com>
2024-06-17 19:06:40 +0100

    check extension activation status

--- api/src/routes.app.ts
+++ api/src/routes.app.ts
@@ -1,7 +1,10 @@
 import { Hono } from "hono";
 import { ApiKey } from "./controllers/app/apiKey";
 import { Profile } from "./controllers/app/profile";
+import { Extenison } from "./controllers/app/extension";
+
 export const router = new Hono();
 
 router.get("/api-key", ApiKey);
 router.get("/profile", Profile);
+router.get("/extension", Extenison);
